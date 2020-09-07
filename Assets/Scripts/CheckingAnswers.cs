72e5b247e0b0d0f24367cdc0e8e3cc79b527a504
Ayoub-Sr-Dev <srayoub95@hotmail.fr>
2020-09-08 00:37:11 +0200

    many changes: scripts related to the quiz scene

---
+++ Assets/Scripts/CheckingAnswers.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckingAnswers : MonoBehaviour
+{
+    //used for detecting the eventclick by the quiz controller!
+    public static bool clicked;
+
+    public void ClickEvent()
+    {
+        clicked = true;
+    }
+
+}
---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Question class !
+public class Question
+{
+    public string questionName { get; set; }
+    public string correctAnswer { get; set; }
+    public string UncorrectAnswer1 { get; set; }
+    public string UncorrectAnswer2 { get; set; }
+    public string UncorrectAnswer3 { get; set; }
+
+    //constructur w/ arguments !
+    public Question(string qName, string cAns, string ans1, string ans2, string ans3)
+    {
+        this.questionName = qName;
+        this.correctAnswer = cAns;
+        this.UncorrectAnswer1 = ans1;
+        this.UncorrectAnswer2 = ans2;
+        this.UncorrectAnswer3 = ans3;
+    }
+
+}
