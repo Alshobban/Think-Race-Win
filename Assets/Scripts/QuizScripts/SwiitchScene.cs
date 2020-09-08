678c13714653e40c718916cc301653f66d128562
Ayoub-Sr-Dev <srayoub95@hotmail.fr>
2020-09-08 03:01:00 +0200

    integrated the canvas with the scripts to the mainquiz scene
    had a test track for testing the quiz functionality , canvs is integrated there where the player colide with a plane but the still need to enable the mouse control for the quiz then still need to add endquiz event

---
+++ Assets/Scripts/QuizScripts/SwiitchScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwiitchScene : MonoBehaviour
+{
+    public GameObject canvas;
+    private void Start()
+    {
+        canvas.gameObject.GetComponent<Canvas>().enabled = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("plane"))
+        {
+            canvas.gameObject.GetComponent<Canvas>().enabled = true;
+        }
+            
+        
+        
+    }
+}
