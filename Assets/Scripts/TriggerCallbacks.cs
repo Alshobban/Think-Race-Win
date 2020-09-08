3989fef4797621ae15fd44aff5a6501a18664758
Gabe <glebzex@mail.ua>
2020-09-08 13:33:26 +0200

    Add trigger callbacks

---
+++ Assets/Scripts/TriggerCallbacks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class TriggerCallbacks : MonoBehaviour
+{
+    public event Action<Collider> TriggerEntered;
+    public event Action<Collider> TriggerExit;
+    public event Action<Collider> TriggerStaying;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TriggerEntered?.Invoke(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        TriggerExit?.Invoke(other);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        TriggerStaying?.Invoke(other);
+    }
+}
