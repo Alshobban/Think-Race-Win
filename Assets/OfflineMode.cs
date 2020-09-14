60b3b46e758a5cbc47c0a4c39f5a726986368f4a
Gabe <glebzex@mail.ua>
2020-09-15 01:24:32 +0200

    Add offline mode enabler

---
+++ Assets/OfflineMode.cs
@@ -0,0 +1,15 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class OfflineMode : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        PhotonNetwork.OfflineMode = true;
+    }
+
+    private void OnDisable()
+    {
+        PhotonNetwork.OfflineMode = false;
+    }
+}
