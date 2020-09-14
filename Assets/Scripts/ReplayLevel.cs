844d4a83134156c73c1d8d3835e2ceb0f6a585dd
Ayoub-Sr-Dev <srayoub95@hotmail.fr>
2020-09-14 17:37:55 +0200

    new changes on the scene
    added replay level by:
    adding ui's for it
    script needed

---
+++ Assets/Scripts/ReplayLevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReplayLevel : MonoBehaviour
+{
+    public GameObject canvas;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Invoke("EnableCanvas", 2f);
+    }
+
+    public void Reset()
+    {
+        canvas.SetActive(false);
+        SceneManager.LoadScene("MainQuiz");
+    }
+
+    public void EnableCanvas()
+    {
+        canvas.SetActive(true);
+    }
+}
