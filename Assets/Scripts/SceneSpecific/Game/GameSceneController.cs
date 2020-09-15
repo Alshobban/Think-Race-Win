506c9e0da945b6088ee6a5233d5be533735029aa
Gabe <glebzex@mail.ua>
2020-09-15 04:20:15 +0200

    Fix slope rotation bug

--- Assets/Scripts/SceneSpecific/Game/GameSceneController.cs
+++ Assets/Scripts/SceneSpecific/Game/GameSceneController.cs
@@ -21,7 +21,10 @@ namespace SceneSpecific.Game
                 return;
 
             var newCar = PhotonNetwork.Instantiate(localPlayerPrefabLocation, startPosition.position,
-                startPosition.rotation);
+                Quaternion.identity);
+
+            newCar.transform.GetChild(0).GetChild(0).Rotate(0f, startPosition.rotation.eulerAngles.y, 0f);
+            Debug.Log(newCar.transform.GetChild(0).rotation.y);
 
             var anchor = newCar.transform.GetChild(0).GetChild(0);
 
