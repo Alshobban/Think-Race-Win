80bbdfffbd9f9f3dd7bbb8180eb04c2eca794553
Gabe <glebzex@mail.ua>
2020-09-15 15:09:45 +0200

    Add room leave button

---
+++ Assets/Scripts/SceneSpecific/RoomSetup/LeaveRoom.cs
@@ -0,0 +1,41 @@
+using NaughtyAttributes;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+using Utilities;
+
+namespace SceneSpecific.RoomSetup
+{
+    public class LeaveRoom : MonoBehaviourPunCallbacks
+    {
+        [SerializeField]
+        private Button backButton;
+
+        [Scene]
+        [SerializeField]
+        private string sceneToLoad;
+
+        private void OnEnable()
+        {
+            backButton.onClick.AddListener(OnLeaveButtonClicked);
+        }
+
+        private void OnDisable()
+        {
+            backButton.onClick.AddListener(OnLeaveButtonClicked);
+        }
+
+        private void OnLeaveButtonClicked()
+        {
+            if (PhotonNetwork.PlayerListOthers.Length > 0)
+            {
+                PhotonNetwork.SetMasterClient(PhotonNetwork.PlayerListOthers.RandomElement());
+            }
+
+            PhotonNetwork.LeaveRoom();
+            PhotonNetwork.AutomaticallySyncScene = false;
+
+            PhotonNetwork.LoadLevel(sceneToLoad);
+        }
+    }
+}
