1f50316dcd418edcace827e48ffc716a02e85a04
Gabe <glebzex@mail.ua>
2020-09-25 18:20:57 +0200

    Remove log

--- Assets/Scripts/SceneSpecific/Game/GameSceneController.cs
+++ Assets/Scripts/SceneSpecific/Game/GameSceneController.cs
@@ -33,7 +33,6 @@ namespace SceneSpecific.Game
             }
 
             newCar.transform.GetChild(0).GetChild(0).Rotate(0f, startPosition.rotation.eulerAngles.y, 0f);
-            Debug.Log(newCar.transform.GetChild(0).rotation.y);
 
             var anchor = newCar.transform.GetChild(0).GetChild(0);
             cinemachineVirtualCamera.Follow = anchor;
