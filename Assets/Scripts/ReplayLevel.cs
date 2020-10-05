8fc010cb91a1668064547c36d6cf0a31b377e137
Ayoub-Sr-Dev <srayoub95@hotmail.fr>
2020-10-05 20:15:40 +0200

    added finish level system after 3 laps

--- Assets/Scripts/ReplayLevel.cs
+++ Assets/Scripts/ReplayLevel.cs
@@ -6,16 +6,24 @@ using UnityEngine.SceneManagement;
 public class ReplayLevel : MonoBehaviour
 {
     public GameObject canvas;
+    private int lapCounter=0;
 
     private void OnTriggerEnter(Collider other)
     {
-        Invoke("EnableCanvas", 2f);
+        lapCounter++;
+        if (lapCounter > 3)
+        {
+            Invoke("EnableCanvas", 1f);
+            other.GetComponent<Car.CarMovement>().enabled = false;
+        }
+        
     }
 
     public void Reset()
     {
+        lapCounter = 0;
         canvas.SetActive(false);
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Main");
     }
 
     public void EnableCanvas()
