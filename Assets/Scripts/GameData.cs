e2a8d114abc04d2638db8a3a28150778b163edf9
Gabe <glebzex@mail.ua>
2020-10-06 15:42:26 +0200

    Move scripts

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,6 @@
+using Questions;
+
+public static class GameData
+{
+    public static QuestionPack CurrentQuestionPack { get; set; }
+}
--- Assets/Scripts/SceneSpecific/Game/GameData.cs
+++
@@ -1,9 +0,0 @@
-using Questions;
-
-namespace SceneSpecific.Game
-{
-    public static class GameData
-    {
-        public static QuestionPack CurrentQuestionPack { get; set; }
-    }
-}
