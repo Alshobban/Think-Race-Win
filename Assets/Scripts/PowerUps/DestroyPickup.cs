311436102909786c294fb6d2fc3e68d4f3719384
Ayoub-Sr-Dev <srayoub95@hotmail.fr>
2020-10-06 21:55:13 +0200

    adding sound folder and changing pickup scripts

--- Assets/Scripts/PowerUps/DestroyPickup.cs
+++ Assets/Scripts/PowerUps/DestroyPickup.cs
@@ -1,41 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class DestroyPickup : MonoBehaviour
 {
-    public GameObject powerupEffectObj;
+    //public GameObject powerupEffectObj;
+    private bool added = false;
+    private float distance= 10.0f;
+    [SerializeField]
+    private string powerupEffectObj = "Explode9";
+
+    private void Start()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().detectCollisions = false;
+        added = true;
+    }
 
     void Update()
     {
-        if (Pickup.added && Input.GetKey(KeyCode.Q))
+        if (added)
         {
-            transform.parent = null;
-            //GetComponent<Rigidbody>().AddForce(transform.forward * 250);
-            GetComponent<Rigidbody>().AddRelativeForce(transform.forward * 250);
-            GetComponent<Rigidbody>().isKinematic = false;
-            GetComponent<SphereCollider>().enabled = true;
-            //
-            Pickup.added = false;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                //transform.SetParent(null);
+                GetComponent<Rigidbody>().useGravity = true;
+                GetComponent<Rigidbody>().detectCollisions = true;
+                GetComponent<Rigidbody>().isKinematic = false;
+                GetComponent<Rigidbody>().AddForce(transform.forward * 350f, ForceMode.Impulse);
+                
+                Pickup.added = false;
+            }
         }
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag != "Player")
         {
-            StartCoroutine(PickupDestroyed(other));
+            //instantiate the effect!
+            Debug.Log("entered");
+            PhotonNetwork.Instantiate(powerupEffectObj, transform.position, transform.rotation);
+            //deactivating the mesh and collider before destroying the gameobject later!
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<SphereCollider>().enabled = false;
+            PhotonNetwork.Destroy(gameObject);
+
+
         }
             
     }
-    IEnumerator PickupDestroyed(Collider playerCollider)
-    {
-        //instantiate the effect!
-        Instantiate(powerupEffectObj, transform.position, transform.rotation);
-        //deactivating the mesh and collider before destroying the gameobject later!
-        GetComponent<MeshRenderer>().enabled = false;
-        GetComponent<SphereCollider>().enabled = false;
+ 
 
-        yield return new WaitForSeconds(2f);
-        Destroy(gameObject);
-    }
 }
