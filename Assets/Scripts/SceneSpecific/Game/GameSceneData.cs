165e241163a3b869acbcc4dd777a1704649460c6
Gabe <glebzex@mail.ua>
2020-10-06 17:34:51 +0200

    Add game scene data

---
+++ Assets/Scripts/SceneSpecific/Game/GameSceneData.cs
@@ -0,0 +1,22 @@
+using Cinemachine;
+using UnityEngine;
+using Utilities;
+
+namespace SceneSpecific.Game
+{
+    public class GameSceneData : SceneData<GameSceneData>
+    {
+        [field: SerializeField]
+        public CinemachineVirtualCamera CinemachineVirtualCamera { get; private set; }
+
+        [SerializeField]
+        private Transform[] startPositions;
+
+        private int _startPositionIndex = 0;
+
+        public Transform GetVacantStartPosition()
+        {
+            return startPositions[_startPositionIndex++ % startPositions.Length];
+        }
+    }
+}
