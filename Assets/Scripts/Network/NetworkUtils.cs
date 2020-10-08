2205ba7ad293686eb5cbf80891e5cd619658417b
Gabe <glebzex@mail.ua>
2020-10-08 12:53:57 +0200

    Fix midterm bug

--- Assets/Scripts/Network/NetworkUtils.cs
+++ Assets/Scripts/Network/NetworkUtils.cs
@@ -12,7 +12,7 @@ namespace Network
             if (PhotonNetwork.NetworkClientState != ClientState.Disconnected)
             {
                 PhotonNetwork.ConnectUsingSettings();
-                PhotonNetwork.ConnectToRegion(NetworkSettings.Instance.ServerRegion);
+                PhotonNetwork.ConnectToRegion("eu");
             }
             else
             {
